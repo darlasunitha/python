853e90aaff21dcc2de101c79c124b0f1c6d6a79e
darlasunitha <42737284+darlasunitha@users.noreply.github.com>
2018-09-24 15:55:06 +0530

    Update 62.py

--- 62.py
+++ 62.py
@@ -1,10 +1,10 @@
-m=raw_input()
-"""if all(c in '01' for c in n):
+p=raw_input()
+"""if all(a in '01' for a in q):
     print "yes"
 else:
     print "no"
     """
-if not(m.translate(None,'01')):
+if not(p.translate(None,'01')):
     print "yes"
 else:
     print "no"
