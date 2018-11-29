f62f2e39731b165c5b7228e6335973d32a30dab2
darlasunitha <42737284+darlasunitha@users.noreply.github.com>
2018-11-29 10:33:30 +0530

    Update 88.py

--- 88.py
+++ 88.py
@@ -1,10 +1,10 @@
-n,m=map(int,raw_input().split())
-if(n>m):
-    min1=n
+a,b=map(int,raw_input().split())
+if(a>b):
+    min1=a
 else:
-    min1=m
+    min1=b
 while(1):
-    if(min1%n==0 and min1%m==0):
+    if(min1%a==0 and min1%b==0):
         print(min1)
         break
     min1=min1+1
